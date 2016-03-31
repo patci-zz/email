139597d24a1afeb0c4e2605321cba1bddf398f3d
Rick Patci <rpatci@gmail.com>
2016-03-31 11:56:30 -0700

    setup basic express route

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
+
+app.use(express.static('./'));
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
