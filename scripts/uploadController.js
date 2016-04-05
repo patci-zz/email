29647b869585e4363f0385eae83cb902c48e2c8f
Rick Patci <rpatci@gmail.com>
2016-04-04 20:56:09 -0700

    update uploadController to return a new object with the key values of user inputs

---
+++ scripts/uploadController.js
@@ -0,0 +1,26 @@
+(function(module) {
+  var uploadController = {};
+
+// As the user fills out form, cache the inputs
+  $('.form').on('change', uploadController.cacheUserInput);
+// We can access these values anytime (ex: `uploadController.cacheUserInput().isbn`)
+  uploadController.cacheUserInput = function() {
+    return {
+      isbn: $('#isbn').val(),
+      authorName: $('#author-name').val(),
+      bookTitle: $('#book-title').val(),
+      chapterOneTitle: $('#chapter-one-title').val(),
+      chapterTwoTitle: $('#chapter-two-title').val(),
+      chapterThreeTitle: $('#chapter-three-title').val(),
+      chapterFourTitle: $('#chapter-four-title').val(),
+      chapterFiveTitle: $('#chapter-five-title').val(),
+      emailIntro: $('textarea').val()
+    };
+  }
+
+    $('.upload-button').on('change', function() {
+      // TODO: call mammoth to convert DOCX file passed in
+    });
+
+  module.uploadController = uploadController;
+})(window);
