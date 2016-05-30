cbfa3fe8b42c3553d84e5ef36e27653a6d3d384d
Gio d'Amelio <giodamelio@gmail.com>
2016-05-30 15:02:39 -0700

    Render chapter inputs with handlebars

--- back/server.js
+++ back/server.js
@@ -3,19 +3,29 @@ const path = require('path');
 const express = require('express');
 const multer = require('multer');
 
+const auth = require('./auth');
+const templates = require('./templates');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
-const auth = require('./auth');
-
+// Simple authentication
 app.use(auth);
-app.use(express.static(path.join(__dirname, '../front')));
 
+app.get('/', (req, res) => {
+  res.send(templates.index({
+    chapters: [1, 2, 3, 4, 5],
+  }));
+});
+
+// Convert word doc to html
 app.post('/convert_file', upload.single('data'), (req, res) => {
   console.log(req.body, req.file);
   res.send('Hello World!');
 });
+
+app.use(express.static(path.join(__dirname, '../front')));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
