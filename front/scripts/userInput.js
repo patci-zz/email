59b94493d735e27c86d0f64bb17ff6dbd5a9cd6b
Jeff Gerber <jallengerber@gmail.com>
2016-08-06 10:20:38 -0700

    refactored object data population to work with new template approach

---
+++ front/scripts/userInput.js
@@ -0,0 +1,35 @@
+// The objects to be populated:
+
+const consistent = {
+  title: 'Sample Title',
+  author: 'Rick Patci',
+  isbn: '123456789',
+  bookCoverImg: 'url',
+  intro: "Well, hello! Welcome to today's First Five!",
+  bannerImgLink: 'http://hccbbooks.com/emails/epicreads/first5/asylum.jpg',
+  bannerHrefLink: 'link',
+  bannerDescription: 'description',
+};
+
+const dynamic = {
+  chapterOne: '1',
+  chapterTwo: '2',
+  chapterThree: '3',
+  chapterFour: '4',
+  chapterFive: '5',
+  chapterBodyOne: 'docx to html - this is the body of the current chapter!',
+  chapterBodyTwo: 'docx to html',
+  chapterBodyThree: 'docx to html',
+  chapterBodyFour: 'docx to html',
+  chapterBodyFive: 'docx to html',
+  audioChapOneLink: 'https://www.google.com',
+  audioChapTwoLink: '',
+  audioChapThreeLink: '',
+  audioChapFourLink: '',
+  audioChapFiveLink: '',
+};
+
+// module.exports = {
+//   consistent: consistent,
+//   dynamic: dynamic,
+// };
