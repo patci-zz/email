1c9729ad3068a54a010ae14172992a9a5f979f1e
Jeff Gerber <jallengerber@gmail.com>
2016-08-08 13:06:15 -0700

    added upload to button

--- front/scripts/uploadController.js
+++ front/scripts/uploadController.js
@@ -4,6 +4,7 @@ $(() => {
   const uploadController = {};
   uploadController.dayNumber = '';
   uploadController.dayInteger = '';
+  uploadController.destinationDirectory = '';
 
   // reads file. Sets dayNumber for element tracking.
   uploadController.readFileInput = function (inputElement, callback) {
@@ -61,7 +62,20 @@ $(() => {
       consistent.bannerHrefLink = $('#bannerHref').val();
       consistent.bannerDescription = $('#bannerDesc').val();
     });
+    $('#downloadTo').on('change', function () {
+      const files = $(this)[0].files;
+      uploadController.destinationDirectory = files[0].path;
+    });
   });
+
+  // $(function () {
+  //       $("#fileDialog").on("change", function () {
+  //           var files = $(this)[0].files;
+  //           for (var i = 0; i < files.length; ++i) {
+  //           console.log(files[i].path);
+  //           }
+  //       });
+  //   });
 
   // // Disable chapter upload unless metadata is complete
   // const metadataInputs = $('#metadata :input');
@@ -95,6 +109,10 @@ $(() => {
       let html = '';
       // this concatenated mess sets up a nice dynamic file name for fs.writeFile:
       let fileName = consistent.title.replace(/\s/g,'-') + '-' + currentTemplate.name + '.html';
+      // add directory path to file name:
+      fileName = uploadController.destinationDirectory + '/' + fileName;
+      alert(fileName);
+
       // the true build compiling begins here:
       for (var props in currentTemplate) {
         // since adding a name property for the new file only, do not include that in the html:
